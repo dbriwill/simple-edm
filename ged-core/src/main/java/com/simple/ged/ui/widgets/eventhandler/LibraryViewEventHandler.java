528e8b98d4f6ae7d3cd222dd62ca65ea1d515b9a
Xavier MICHEL <xavier.michel.mx440@gmail.com>
2013-01-27 17:05:31 +0100

    Cleaning code

--- ged-core/src/main/java/com/simple/ged/ui/widgets/eventhandler/LibraryViewEventHandler.java
+++ ged-core/src/main/java/com/simple/ged/ui/widgets/eventhandler/LibraryViewEventHandler.java
@@ -496,7 +496,7 @@ public class LibraryViewEventHandler implements Callback<TreeView<String>,TreeCe
 		@Override
 		public void cancelEdit() {
 			super.cancelEdit();
-			setText((String) getItem());
+			setText(getItem());
 			setGraphic(getTreeItem().getGraphic());
 		}
 
@@ -573,7 +573,7 @@ public class LibraryViewEventHandler implements Callback<TreeView<String>,TreeCe
 		}
 
 		private String getString() {
-			return getItem() == null ? "" : getItem().toString();
+			return getItem() == null ? "" : getItem();
 		}
 	}
 
@@ -614,7 +614,7 @@ public class LibraryViewEventHandler implements Callback<TreeView<String>,TreeCe
 				
 				// we create the child if it doesn't exists
 				if (! hasChild(child, item)) {
-					child.getChildren().add(new TreeItem<String>(LibraryView.convertToNodeName(item), libraryView.get().getIconForNode(relativeToAbsolultPath(stack + item))));
+					child.getChildren().add(new TreeItem<>(LibraryView.convertToNodeName(item), libraryView.get().getIconForNode(relativeToAbsolultPath(stack + item))));
 				}
 				
 				// get the child
