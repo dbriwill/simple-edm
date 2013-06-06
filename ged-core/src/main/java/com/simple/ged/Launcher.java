094bd6a3938468d4e67df3515a72920d54d32b16
Xavier MICHEL <xavier.michel.mx440@gmail.com>
2013-06-06 20:27:29 +0200

    Fixes #53

--- ged-core/src/main/java/com/simple/ged/Launcher.java
+++ ged-core/src/main/java/com/simple/ged/Launcher.java
@@ -1,23 +1,15 @@
 package com.simple.ged;
 
 
-import java.io.File;
-import java.util.Map.Entry;
-
-import com.simple.ged.services.ElasticSearchService;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.simple.ged.services.ElasticSearchService;
 import com.simple.ged.ui.MainWindow;
-import com.simple.ged.update.DoUpdate;
-import com.simple.ged.update.UpdateHelper;
 import com.simple.ged.update.UpdateInformations;
 
-import fr.xmichel.javafx.dialog.Dialog;
 import fr.xmichel.toolbox.hibernate.sqlite.HibernateUtil;
 import fr.xmichel.toolbox.tools.PropertiesHelper;
 
@@ -41,6 +33,8 @@ public final class Launcher {
 	
 	public static void main(String[] args) {
 				
+		logger.info("loading properties...");
+		
 		// load properties
 		PropertiesHelper.getInstance().load("properties/strings.properties");
 		PropertiesHelper.getInstance().load("properties/icons.properties");
@@ -50,93 +44,24 @@ public final class Launcher {
 		PropertiesHelper.getInstance().load(UpdateInformations.CONSTANT_PROPERTIES_FILE_PATH);
 		
 		// create or update database
+		logger.info("open hibernate session...");
 		HibernateUtil.getSessionFactory().openSession();
 		
-		
+		logger.info("complete update...");
 		MiddleProfile.getInstance().completeUpdate();
 
-
         // launch document indexation
-        new Thread(new Runnable() {
+		logger.info("launch non indexed-docs indexation...");
+		new Thread(new Runnable() {
             @Override
             public void run() {
                 ElasticSearchService.indexAllNonIndexedDocumentInLibrary();
             }
         }).start();
 
-
-		
-		// check for updates
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				
-				/*
-				 * Update the updater if necessary
-				 */
-				
-				String onlineUpdaterVersion = UpdateHelper.getVersionNumber(UpdateInformations.UPDATER_UPDATE_DESCRIPTOR_PATH);
-				
-				if (Float.parseFloat(onlineUpdaterVersion) <= Float.parseFloat(DoUpdate.UPDATER_VERSION)) {
-					logger.info("Updater : no new version");
-				}
-				else {
-					logger.info("Updater need to be updated");
-					
-					for (Entry<String, String> e : UpdateHelper.getFilesToDownloadMap(UpdateInformations.UPDATER_UPDATE_DESCRIPTOR_PATH).entrySet()) {
-						logger.debug(e.getKey() + " => " + System.getProperty("user.dir") + File.separator + e.getValue());
-						try {
-							UpdateHelper.downloadAndReplaceFile(e.getKey(), System.getProperty("user.dir") + File.separator + e.getValue());
-						} 
-						catch (Exception e2) {
-							logger.error("Error while downloading : " + e.getKey(), e2);
-						}
-					}
-				}
-				
-				
-				/*
-				 * Now update the core if necessary
-				 */
-				
-				String onlineVersion = UpdateHelper.getVersionNumber(UpdateInformations.GED_CORE_UPDATE_DESCRIPTOR_PATH);
-				
-				if (Float.parseFloat(onlineVersion) <= Float.parseFloat(PropertiesHelper.getInstance().getProperties().getProperty("APPLICATION_VERSION"))) {
-					logger.info("UpdateManager : No new version");
-					return;
-				}
-				
-				logger.info("UpdateManager : New version (" + onlineVersion + ") is avaliable");
-				
-				if (onlineVersion != null) {
-					
-					Dialog.buildConfirmation("Nouvelle version disponible",
-							"Vous utilisez la version " + PropertiesHelper.getInstance().getProperties().getProperty("APPLICATION_VERSION") 
-							+ " de Simple GED or la version " + onlineVersion + " est disponible.\n"
-							+ "Voulez-vous faire la mise à jour ? (recommandé)"
-					)
-			        .addYesButton(new EventHandler<ActionEvent>() {
-			            @Override
-			            public void handle(ActionEvent arg0) {
-			            	try {
-						        Runtime.getRuntime().exec("java -jar simpleGedUpdateSystem.jar");
-						        System.exit(0);
-							} catch (Exception e) {
-								logger.error("Cannot do upgrade : ", e);
-								Dialog.showThrowable("Erreur", "Impossible de lancer l'assistant de mise à jour", e);
-							}
-			            }
-			        })
-			        .addNoButton(null)
-			        .addCancelButton(null)
-			        .build()
-			        .show();
-				}
-			}
-		}).start();
-	
-	
+        
 		// The main window
+		logger.info("show main window !");
 		Application.launch(MainWindow.class, args);
 	}
 
--- ged-core/src/main/java/com/simple/ged/ui/MainWindow.java
+++ ged-core/src/main/java/com/simple/ged/ui/MainWindow.java
@@ -1,11 +1,16 @@
 package com.simple.ged.ui;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Map.Entry;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -28,7 +33,11 @@ import com.simple.ged.ui.screen.SoftwareScreen;
 import com.simple.ged.ui.screen.ToolBar;
 import com.simple.ged.ui.screen.WorkerPluginConfigurationScreen;
 import com.simple.ged.ui.screen.WorkerPluginScreen;
+import com.simple.ged.update.DoUpdate;
+import com.simple.ged.update.UpdateHelper;
+import com.simple.ged.update.UpdateInformations;
 
+import fr.xmichel.javafx.dialog.Dialog;
 import fr.xmichel.toolbox.tools.FileHelper;
 import fr.xmichel.toolbox.tools.PropertiesHelper;
  
@@ -117,6 +126,9 @@ public class MainWindow extends Application {
 		
 		// launch plugin update... (threaded)
 		PluginManager.launchGetterPluginUpdate(new FakeScreen(this));
+		
+		// launch upgrade check... (threaded)
+		checkForUpgrade();
     }
     
 
@@ -256,6 +268,79 @@ public class MainWindow extends Application {
 
 	public boolean isNotOnHomeScreen() {
 		return ! (currentCentralScreen instanceof LibraryViewScreen);
+	}
+	
+	
+	
+	private void checkForUpgrade() {
+		// check for updates
+        Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				
+				/*
+				 * Update the updater if necessary
+				 */
+				
+				String onlineUpdaterVersion = UpdateHelper.getVersionNumber(UpdateInformations.UPDATER_UPDATE_DESCRIPTOR_PATH);
+				
+				if (Float.parseFloat(onlineUpdaterVersion) <= Float.parseFloat(DoUpdate.UPDATER_VERSION)) {
+					logger.info("Updater : no new version");
+				}
+				else {
+					logger.info("Updater need to be updated");
+					
+					for (Entry<String, String> e : UpdateHelper.getFilesToDownloadMap(UpdateInformations.UPDATER_UPDATE_DESCRIPTOR_PATH).entrySet()) {
+						logger.debug(e.getKey() + " => " + System.getProperty("user.dir") + File.separator + e.getValue());
+						try {
+							UpdateHelper.downloadAndReplaceFile(e.getKey(), System.getProperty("user.dir") + File.separator + e.getValue());
+						} 
+						catch (Exception e2) {
+							logger.error("Error while downloading : " + e.getKey(), e2);
+						}
+					}
+				}
+				
+				
+				/*
+				 * Now update the core if necessary
+				 */
+				
+				String onlineVersion = UpdateHelper.getVersionNumber(UpdateInformations.GED_CORE_UPDATE_DESCRIPTOR_PATH);
+				
+				if (Float.parseFloat(onlineVersion) <= Float.parseFloat(PropertiesHelper.getInstance().getProperties().getProperty("APPLICATION_VERSION"))) {
+					logger.info("UpdateManager : No new version");
+					return;
+				}
+				
+				logger.info("UpdateManager : New version (" + onlineVersion + ") is avaliable");
+				
+				if (onlineVersion != null) {
+					
+					Dialog.buildConfirmation("Nouvelle version disponible",
+							"Vous utilisez la version " + PropertiesHelper.getInstance().getProperties().getProperty("APPLICATION_VERSION") 
+							+ " de Simple GED or la version " + onlineVersion + " est disponible.\n"
+							+ "Voulez-vous faire la mise à jour ? (recommandé)"
+					)
+			        .addYesButton(new EventHandler<ActionEvent>() {
+			            @Override
+			            public void handle(ActionEvent arg0) {
+			            	try {
+						        Runtime.getRuntime().exec("java -jar simpleGedUpdateSystem.jar");
+						        System.exit(0);
+							} catch (Exception e) {
+								logger.error("Cannot do upgrade : ", e);
+								Dialog.showThrowable("Erreur", "Impossible de lancer l'assistant de mise à jour", e);
+							}
+			            }
+			        })
+			        .addNoButton(null)
+			        .addCancelButton(null)
+			        .build()
+			        .show();
+				}
+			}
+		});
 	}
 	
 }
