c83ccb4dbe62f37de19d09f9a72967deb395ac6c
Xavier MICHEL <xavier.michel.mx440@gmail.com>
2013-09-30 20:19:18 +0200

    Fixes #65 and refactor changelog writer

--- ged-core/src/main/java/com/simple/ged/MiddleProfile.java
+++ ged-core/src/main/java/com/simple/ged/MiddleProfile.java
@@ -7,11 +7,13 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Lists;
 import com.simple.ged.models.GedMessage;
 import com.simple.ged.services.GedMessageService;
 import com.simple.ged.tools.SpringFactory;
@@ -74,54 +76,17 @@ public final class MiddleProfile implements Serializable {
 
 		Properties properties = PropertiesHelper.getInstance().getProperties();
 
-		// changes on version 3.3
-		if (lastKnownVersion < 3.3) {
-			logger.info("Completing update to version 3.3 ...");
-			// add update informations message
-			gedMessageService.save(new GedMessage("NEUTRAL", properties.getProperty("update_33_msg")));
-			lastKnownVersion = 3.3;
-		}
-
-		// changes on version 3.4
-		if (lastKnownVersion < 3.4) {
-			logger.info("Completing update to version 3.4 ...");
-			// add update informations message
-			gedMessageService.save(new GedMessage("NEUTRAL", properties.getProperty("update_34_msg")));
-			lastKnownVersion = 3.4;
-		}
-
-		// changes on version 4.0
-		if (lastKnownVersion < 4.0) {
-			logger.info("Completing update to version 4.0 ...");
-			// add update informations message
-			gedMessageService.save(new GedMessage("NEUTRAL", properties.getProperty("update_40_msg")));
-			lastKnownVersion = 4.0;
-		}
-
-		// changes on version 4.1
-		if (lastKnownVersion < 4.1) {
-			logger.info("Completing update to version 4.1 ...");
-			// add update informations message
-			gedMessageService.save(new GedMessage("NEUTRAL", properties.getProperty("update_41_msg")));
-			lastKnownVersion = 4.1;
-		}
-
-		// changes on version 4.2
-		if (lastKnownVersion < 4.2) {
-			logger.info("Completing update to version 4.2 ...");
-			// add update informations message
-			gedMessageService.save(new GedMessage("NEUTRAL", properties.getProperty("update_42_msg")));
-			lastKnownVersion = 4.2;
-		}
-
-		// changes on version 4.3
-		if (lastKnownVersion < 4.3) {
-			logger.info("Completing update to version 4.3 ...");
-			// add update informations message
-			gedMessageService.save(new GedMessage("NEUTRAL", properties.getProperty("update_43_msg")));
-			lastKnownVersion = 4.3;
-		}
+		List<Double> versionsWithChangelog = Lists.newArrayList(
+				3.3, 3.4, 4.0, 4.1, 4.2, 4.3, 4.4
+		);
 		
+		for (Double newVersion : versionsWithChangelog) {
+			if (lastKnownVersion < newVersion) {
+				logger.info("Completing update to version {} ...", newVersion);
+				gedMessageService.save(new GedMessage("NEUTRAL", properties.getProperty("update_" + String.valueOf(lastKnownVersion).replaceFirst("\\.", "") + "_msg")));
+				lastKnownVersion = newVersion;
+			}
+		}
 		
 		// save new known version
 		lastKnownVersion = Double.parseDouble(properties.getProperty("APPLICATION_VERSION"));
