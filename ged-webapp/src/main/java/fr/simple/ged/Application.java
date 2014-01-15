ded92c1193e3c548126daddd3a52c69f517db799
Xavier MICHEL <xavier.michel.mx440@gmail.com>
2014-01-15 19:27:38 +0100

    Clean

--- ged-webapp/src/main/java/fr/simple/ged/Application.java
+++ ged-webapp/src/main/java/fr/simple/ged/Application.java
@@ -1,29 +1,10 @@
 package fr.simple.ged;
 
-import java.util.Arrays;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-
-import fr.simple.ged.service.ElasticSearchService;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.util.StringUtils;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.thymeleaf.spring3.SpringTemplateEngine;
-import org.thymeleaf.spring3.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 
 @EnableAutoConfiguration
@@ -40,7 +21,7 @@ public class Application {
         logger.info("       --enable-embedded-storage  | -e");
         logger.info("       --help                     | -h");
     }
-
+    
 
 	public static void main(String[] args) {
 
@@ -67,8 +48,8 @@ public class Application {
 
         // hello !
         logger.info("==========================================================================");
-        //logger.info("Hi, this is {} version {}", PropertiesHelper.getInstance().getProperties().get("APPLICATION_NAME"), PropertiesHelper.getInstance().getProperties().get("APPLICATION_VERSION"));
-        //logger.info("You can report issues on {}", PropertiesHelper.getInstance().getProperties().get("APPLICATION_ISSUES_URL"));
+//        logger.info("Hi, this is {} version {}", constants.get("APPLICATION_NAME"), constants.get("APPLICATION_VERSION"));
+//        logger.info("You can report issues on {}", constants.get("APPLICATION_ISSUES_URL"));
         logger.info("--------------------------------------------------------------------------");
         logger.info("java.runtime.name          : " + System.getProperty("java.runtime.name"));
         logger.info("java.runtime.version       : " + System.getProperty("java.runtime.version"));
--- ged-webapp/src/main/java/fr/simple/ged/WebConfig.java
+++ ged-webapp/src/main/java/fr/simple/ged/WebConfig.java
@@ -3,7 +3,6 @@ package fr.simple.ged;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.util.StringUtils;
@@ -15,9 +14,6 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
-import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring3.SpringTemplateEngine;
 import org.thymeleaf.spring3.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
